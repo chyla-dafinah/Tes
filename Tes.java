f7de1722f648bfc96b8f2253cec993b4c947e253
Cilaaa <chyla_amalia_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 14:08:10 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,8 @@
+public class Tes {
+    public static void main(String[] args) {
+            String nama = "Chyla";
+            String kelas = "X RPL 5";
+            System.out.println("Halo, nama saya " + nama + " dari kelas " + kelas);
+            
+    }
+}
